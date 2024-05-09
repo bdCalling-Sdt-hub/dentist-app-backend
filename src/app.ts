c55f4321093ee3621c96ad7ed06c124e2386c191
siffahim <siffahim25@gmail.com>
2024-05-09 18:49:00 +0600

    feat: :sparkles: user route and folder created

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express, { Application, Request, Response } from "express";
+import router from "./routes";
 const app: Application = express();
 
+//app router
+app.use("/api/v1", router);
+
+//server response
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Dentist");
+  res.send(
+    "<h1 style='font-family: Arial, sans-serif;text-align:center;color:#12354E'>Hey! I m front man of dentist, how can i assist you!</h1>"
+  );
 });
+
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { UserRoutes } from "../app/modules/user/user.route";
+const router = express.Router();
+
+const appRouteList = [
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+];
+
+appRouteList.forEach((route) => router.use(route.path, route.route));
+
+export default router;
