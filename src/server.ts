7ffa671226e62d986a83c28a55b555bc52884dc9
siffahim <siffahim25@gmail.com>
2024-05-09 17:56:14 +0600

    feat: :boom: dot env setting updated

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,53 @@
 import colors from "colors";
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
+import { errorLogger, logger } from "./shared/logger";
+
+//uncaught exception
+process.on("uncaughtException", (error) => {
+  errorLogger.error(error);
+  process.exit(1);
+});
 
 let server: any;
 async function main() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/dentist-app");
-    console.log(colors.green("♻️  Database connected successfully"));
+    await mongoose.connect(config.database_url as string);
+    logger.info(colors.green("♻️  Database connected successfully"));
+
+    const port =
+      typeof config.port === "number" ? config.port : parseInt(config.port!);
 
     //app listen here
-    server = app.listen(5001, () => {
-      console.log(colors.yellow(`🚀 Application Running on port:${5001}`));
+    server = app.listen(port, config.ip_address as string, () => {
+      logger.info(
+        colors.yellow(`🚀 Application Running on port:${config.port}`)
+      );
     });
   } catch (error) {
-    console.log(error);
+    errorLogger.error(error);
   }
+
+  //unhandle rejection
+  process.on("unhandledRejection", (error) => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
 main();
+
+//sigterm
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM IS RECEIVE");
+  if (server) {
+    server.close();
+  }
+});
